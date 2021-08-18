75ac6dad8eeaad8d35917811f708b5ec93d8c8f3
James <jameschen625@gmail.com>
2021-08-18 15:29:35 -0400

    cleaned up extraneous code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import GameView from "./scripts/gameview";
 
-
 document.addEventListener("DOMContentLoaded", (event) => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
-
-    // ctx.fillStyle = "#ADD8E6";
-    // ctx.fillRect(0, 0, 1000, 500);
-    // const game = new Game(canvas, ctx, 25, "blue");
-    // const start = document.querySelector(".start");
-    // start.addEventListener("click", game.start.bind(game))
-    // canvas.addEventListener("mousemove", (event) => {
-    //     canvas.style.backgroundPositionX = event.offsetX + "px";
-    //     canvas.style.backgroundPositionY = event.offsetY + "px";
-    // });
-
-    // canvas.addEventListener("mouseenter", () => {
-    //     setTimeout( () =>{
-    //         canvas.style.transition = 0;
-    //         canvas.removeEventListener("mouseenter");
-    //     }, 250)
-    // })
-
 
     const newGame = new GameView(canvas, ctx, 25, "red");
     newGame.start();
